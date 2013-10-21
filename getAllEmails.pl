ecf493ca8e53e8f8420db27daa3a2a08fa474369
Dan Rumney <dancrumb@gmail.com>
2013-10-20 19:42:28 -0500

    Updated  - getAllEmails to provide details and return in JSON  - message.txt to describe new approach

--- getAllEmails.pl
+++ getAllEmails.pl
@@ -7,13 +7,24 @@ use Carp;
 use Readonly;
 use Data::Dumper;
 use Underscore;
+use JSON;
 
-my $email = NerdNite::Email->new(1);
+my $email = NerdNite::Email->new();
+
+my $details = shift || 0;
 
 my $pops     = $email->request('listpops');
 my $forwards = $email->request('listforwards');
 
-my $emails = _->union(_->pluck($pops, 'email'), _->pluck($forwards, 'dest'));
-$emails = _->sort($emails);
+my $emails = [];
 
-print Dumper($emails);
+if ($details) {
+    push @$emails, $pops;
+    push @$emails, $forwards;
+}
+else {
+    $emails = _->union(_->pluck($pops, 'email'), _->pluck($forwards, 'dest'));
+    $emails = _->sort($emails);
+}
+
+print to_json($emails);
