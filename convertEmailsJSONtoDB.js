6ad5f73549d78c61605129a3df2a23330eb76272
Dan Rumney <dancrumb@gmail.com>
2014-02-02 18:13:01 -0500

    Added DB saving code

--- convertEmailsJSONtoDB.js
+++ convertEmailsJSONtoDB.js
@@ -1,8 +1,15 @@
 #!/usr/bin/env nodejs
 (function() {
+    "use strict";
     var MongoClient = require("mongodb").MongoClient,
         _           = require("lodash"),
-        emails      = require("./emails.020214.json");
+        emails      = require("./emails.020214.json"),
+        displayInfo = function (doc) {
+            console.log(doc);
+        },
+        saveDoc     = function () {
+            console.warn("No DB connection so not saving doc");
+        };
         
         MongoClient.connect("mongodb://nerdnite:s4tgd1tw@localhost/nerdnite",
             function(err, db) {
@@ -10,13 +17,35 @@
                     console.error("Failed to connect: ", err);
                 }
                 else {
-                    console.log("Connected to db");
-                    _.forEach(emails, function(targets, source) {
-                        console.log(source + " redirects to ", targets);
-                    })
-                    
-                    db.close();
+                    var forwards = db.collection("forwards");
+                    saveDoc = function (doc) {
+                        console.log("Saving doc...");
+                        forwards.update(
+                            { "_id": doc.id },
+                            doc,
+                            { upsert: true }
+                        );
+                    };
                 }
+                //else {
+                    
+                    _.forEach(emails, function(targets, source) {
+                        var id = source.split('@')[0],
+                            doc = {
+                                _id: id,
+                                source: source,
+                                targets: targets
+                            }
+                        displayInfo(doc);
+                        saveDoc(doc);
+                        if(db) {
+                            
+                        }
+                    })
+                    if (db) {
+                        db.close();
+                    }
+                //}
             }
         );
         
