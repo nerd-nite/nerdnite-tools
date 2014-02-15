1b1c6af2d6b723a9a0bac3a5e6e3d0fee10db605
Dan Rumney <dan@nerdnite.com>
2014-02-15 01:11:44 +0000

    Name generator

---
+++ createNames.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node 
+/*
+ * Report the Bosses associated with a city
+ *
+ */
+(function (){
+    "use strict";
+    var MongoClient     = require("mongodb").MongoClient,
+        _               = require("lodash");
+        
+    
+    MongoClient.connect("mongodb://nerdnite:s4tgd1tw@nerdnite2.com/nerdnite",
+        function(err, db) {
+            var forwards,
+                errorOut = function () {
+                        console.error.apply(console, arguments);
+                        db.close();
+                        process.exit(1);
+                };
+                
+            if(err) {
+                console.error("Failed to connect: ", err);
+            }
+            else {
+                console.info("Connected to DB");
+                forwards = db.collection("forwards");
+                forwards.find().each(function(err,fwd){
+                    if(!fwd) {
+
+                        db.close();
+                        return;
+                    }
+                   
+		    if(fwd.name) {
+                console.log("Already have a name for "+
+fwd.name);
+            }
+            else {
+                var newName = fwd._id.split(".").join(" ");
+                forwards.update(fwd, { $set: { name: newName } }, function (err,result){
+                    if(err) {
+                        console.error("ERR:",error);
+                    }
+                    else {
+                        console.log("Success:", result);
+                    }
+                });
+            }
+        }
+    );
+}());
