d374632aa4f16bfbcbb6975396a26ae71de722df
Dan Rumney <dancrumb@gmail.com>
2014-06-25 23:46:37 +0000

    Updates to correct alias template and incorrect Mongo location

--- createAliasMap.js
+++ createAliasMap.js
@@ -56,7 +56,7 @@
         
     }
 
-    MongoClient.connect("mongodb://nerdnite:s4tgd1tw@nerdnite2.com/nerdnite",
+    MongoClient.connect("mongodb://nerdnite:s4tgd1tw@mail.nerdnite2.com/nerdnite",
         function(err, db) {
             var bosses  = !db ? null : db.collection("bosses"),
                 cities  = !db ? null : db.collection("cities"),
